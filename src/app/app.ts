de0844b8b288eddc592ba864a6482ca9e7246c2a
Sereyusaphea <sereyusaphea68@gmail.com>
2025-08-20 23:52:31 +0700

    a

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from './navbar/navbar';
+import {HttpClientModule} from '@angular/common/http';
+import { ProductList } from "./product-list/product-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, HttpClientModule, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('inputdata');
+
 }
